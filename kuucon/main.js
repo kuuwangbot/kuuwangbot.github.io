d3a16fa608c5a876bb60badba0813406279aa024
Kuuw <44305997+kuuwangbot@users.noreply.github.com>
2023-06-16 12:20:16 +0900

    메인 js 재재재수정

--- kuucon/main.js
+++ kuucon/main.js
@@ -1,6 +1,14 @@
 fetch('../img/emoji.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json);
+    processJsonData(json); // Call a separate function to process the JSON data
+  })
+  .catch((error) => {
+    console.log('Error loading JSON file:', error);
+  });
 
-
-console.log(json[1])
+function processJsonData(json) {
+  console.log(json[1]); // Accessing json[1] outside the fetch() call
+  // Other code that uses the JSON data
+}
